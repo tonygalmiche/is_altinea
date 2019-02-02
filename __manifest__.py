0bfed8f3d098b07d84c6d2c33e2de7a363be7eee
Tony Galmiche <tony.galmiche@infosaone.com>
2019-02-02 14:20:55 +0100

    demandes de ce jour

--- __manifest__.py
+++ __manifest__.py
@@ -1,28 +1,30 @@
 # -*- coding: utf-8 -*-
 {
-    'name'     : 'InfoSaône - Module Odoo pour ALTINEA',
+    'name'     : u'InfoSaône - Module Odoo pour ALTINEA',
     'version'  : '0.1',
-    'author'   : 'InfoSaône',
-    'category' : 'InfoSaône',
+    'author'   : u'InfoSaône',
+    'category' : u'InfoSaône',
 
 
-    'description': """
+    'description': u"""
 InfoSaône - Module Odoo pour ALTINEA
 ===================================================
 """,
-    'maintainer' : 'InfoSaône',
+    'maintainer' : u'InfoSaône',
     'website'    : 'http://www.infosaone.com',
     'depends'    : [
         'base',
         'account',
         'account_accountant',
         'document',
+        'professional_templates',
 ],
     'data' : [
         'security/ir.model.access.csv',
         'views/assets.xml',
         'views/is_export_compta_view.xml',
         'views/sale_view.xml',
+        'views/account_invoice_view.xml',
         'views/account_analytic_account_view.xml',
     ],
     'installable': True,
