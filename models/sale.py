fa8517b15e3a271f4d188ab82fe0edaa85eafdcc
Tony Galmiche <tony.galmiche@infosaone.com>
2019-02-17 07:33:57 +0100

    reprise project_title commande sur facture

--- models/sale.py
+++ models/sale.py
@@ -9,3 +9,12 @@ class SaleOrderLine(models.Model):
     is_commentaire = fields.Text(string="Commentaire")
 
 
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    @api.multi
+    def _prepare_invoice(self):
+        vals = super(SaleOrder, self)._prepare_invoice()
+        vals['project_title'] = self.project_title or ''
+        return vals
+
